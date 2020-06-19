b69a81e2473d67447659ac74f1a1e69b2257dc24
Krisztian Gulyas <gulyas.krisztian1o9@gmail.com>
2020-06-19 20:07:59 +0200

    Extract dictionary access in history.cljs

--- src/pomodoro/history.cljs
+++ src/pomodoro/history.cljs
@@ -8,22 +8,22 @@
 
 (defn history-table [state]
   [:div#history
-   [:h3 (get-in @state [:dictionary :history])]
+   [:h3 (action/dict state :history)]
    (when (:active @state) [:div
                            (ui/control-buttons state)
                            [:p]])
    (let [history (storage/get-history)
          full-width (min 600 ((:width @state) 0.94))
          width (* full-width 0.2)
-         str-delete (if (< 550 full-width) (get-in @state [:dictionary :remove]) "Delete")]
+         str-delete (if (< 550 full-width) (action/dict state :remove) "Delete")]
      (when (seq? history)
        [:table {:class "table table-striped table-bordered" :id "history" :style {:width full-width}}
         [:thead {:class "thead-dark"}
          [:tr
-          [:th (get-in @state [:dictionary :task-name])]
-          (when (< 550 full-width)[:th (get-in @state [:dictionary :start-time])])
-          [:th (get-in @state [:dictionary :planed-duration])]
-          [:th (get-in @state [:dictionary :real-duration])]
+          [:th (action/dict state :task-name)]
+          (when (< 550 full-width)[:th (action/dict state :start-time)])
+          [:th (action/dict state :planed-duration)]
+          [:th (action/dict state :real-duration)]
           [:th (ui/button-element (@state :active) width str-delete storage/delete-history)]]]
         (into [:tbody]
               (for [task history]
@@ -32,4 +32,4 @@
                  (when (< 550 full-width)[:td (.toLocaleString (js/Date. (:start task)))])
                  [:td (tf/render-time (* 1000 (:length task)))]
                  [:td (tf/render-time (:duration task))]
-                 [:td (ui/button-element (@state :active) width (get-in @state [:dictionary :restart]) #(action/restart state task))]]))]))])
+                 [:td (ui/button-element (@state :active) width (action/dict state :restart) #(action/restart state task))]]))]))])
